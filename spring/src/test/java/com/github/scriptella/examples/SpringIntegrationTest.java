baa2c4c4f2b2623c75a0c84af5fae515af2eb1a2
ejboy <fyodor.kupolov@gmail.com>
2012-11-07 12:08:07 +0100

    Added Spring integration example.

---
+++ spring/src/test/java/com/github/scriptella/examples/SpringIntegrationTest.java
@@ -0,0 +1,28 @@
+package com.github.scriptella.examples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import scriptella.driver.spring.EtlExecutorBean;
+import scriptella.execution.EtlExecutorException;
+
+/**
+ * Just a smoke test which
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/spring-beans.xml"})
+public class SpringIntegrationTest {
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Test
+    public void test() throws EtlExecutorException {
+        //Executes populate-db.etl.xml job
+        //DB schema has already been created in create-db.etl.xml at context initialization time
+        EtlExecutorBean executorBean = applicationContext.getBean("populateDb", EtlExecutorBean.class);
+        executorBean.execute();
+    }
+}
